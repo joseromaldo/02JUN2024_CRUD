a971a937bc03066713d538234a425aa9fd0569ea
Maldonado <joseromaldo.1997@gmail.com>
2024-06-02 19:44:35 -0600

    Creación guardar para ventas

---
+++ controladores/ventas/guardar.php
@@ -0,0 +1,19 @@
+<?php
+
+    require '../../modelos/Encabezado.php';
+    
+    // VALIDAR INFORMACION
+    $_POST['fact_cliente'] = filter_var( $_POST['fact_cliente'] , FILTER_SANITIZE_NUMBER_INT);
+    $regex = '/^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/';
+
+    $_POST['fact_fecha'] = filter_var($_POST['fact_fecha'], FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => $regex]]);
+ 
+
+    if($_POST['fact_cliente'] == '' || !$_POST['fact_fecha'] ){
+        $resultado = [
+            'mensaje' => 'DEBE VALIDAR LOS DATOS',
+            'codigo' => 2
+        ];
+    }else{
+        // var_dump($_POST);
+    }
