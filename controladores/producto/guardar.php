56e1e1e71285504f49e415416f194ae79b7f851e
Maldonado <joseromaldo.1997@gmail.com>
2024-06-02 19:42:42 -0600

    Creación de buscar, eliminar, guardar y modificar para producto

---
+++ controladores/producto/guardar.php
@@ -0,0 +1,62 @@
+<?php 
+
+    require '../../modelos/Producto.php';
+  
+    $mensaje = '';
+    $precio =  $_POST['prod_precio'];
+    // VALIDAR INFORMACION
+    $_POST['prod_nombre'] = htmlspecialchars( $_POST['prod_nombre']);
+    $_POST['prod_precio'] = filter_var( $precio , FILTER_VALIDATE_FLOAT) ;
+
+    
+    if($_POST['prod_nombre'] == '' || !$_POST['prod_precio'] || $_POST['prod_precio'] < 0 ){
+        // ALERTA PARA VALIDAR DATOS
+        $resultado = [
+            'mensaje' => 'DEBE VALIDAR LOS DATOS',
+            'codigo' => 2
+        ];
+    }else{
+        try {
+            // REALIZAR CONSULTA
+            $producto = new Producto($_POST);
+            $guardar = $producto->guardar();
+            $resultado = [
+                'mensaje' => 'PRODUCTO INSERTADO CORRECTAMENTE',
+                'codigo' => 1
+            ];
+            
+        } catch (PDOException $pe){
+            $resultado = [
+                'mensaje' => 'OCURRIO UN ERROR INSERTANDO A LA BD',
+                'detalle' => $pe->getMessage(),
+                'codigo' => 0
+            ];
+        } catch (Exception $e) {
+            $resultado = [
+                'mensaje' => 'OCURRIO UN ERROR EN LA EJECUCIÓN',
+                'detalle' => $e->getMessage(),
+                'codigo' => 0
+            ];
+        }
+        
+    }
+
+    $alertas = ['danger', 'success', 'warning'];
+
+    
+    include_once '../../vistas/templates/header.php'; ?>
+
+    <div class="row justify-content-center">
+        <div class="col-lg-6 alert alert-<?=$alertas[$resultado['codigo']] ?>" role="alert">
+            <?= $resultado['mensaje'] ?>
+            <?= $resultado['detalle'] ?>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <a href="../../vistas/producto/index.php" class="btn btn-primary w-100">Volver al formulario</a>
+        </div>
+    </div>
+
+
+<?php include_once '../../vistas/templates/footer.php'; ?>  
